82d46a12b1e0a9a5b62e40ea40b1b481cf534f7e
Martin Wedvich <wedvich@gmail.com>
2013-11-21 15:12:25 +0100

    Added the click logic, and fixed IE8 support.

--- knockout.tablesort.js
+++ knockout.tablesort.js
@@ -2,6 +2,35 @@
 
 ( function ( undefined ) {
     
+    // Array.prototype.map polyfill from Mozilla Developer Network
+    // Full version with comments:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Compatibility
+    if ( !Array.prototype.map ) {
+        Array.prototype.map = function ( callback, thisArg ) {
+            var T, A, k;
+            var O = Object( this );
+            var len = O.length >>> 0;
+            if ( typeof callback !== "function" ) {
+                throw new TypeError( callback + " is not a function" );
+            }
+            if ( thisArg ) {
+                T = thisArg;
+            }
+            A = new Array( len );
+            k = 0;
+            while ( k < len ) {
+                var kValue, mappedValue;
+                if ( k in O ) {
+                    kValue = O[k];
+                    mappedValue = callback.call( T, kValue, k, O );
+                    A[k] = mappedValue;
+                }
+                k++;
+            }
+            return A;
+        };
+    }
+    
     // Helper function to recursively find the closest parent
     // element that matches the target node name, including the
     // element itself.
@@ -17,7 +46,50 @@
             return element;
 
         return closestParentOrSelf( element, target );
-    };    
+    };
+    
+    // Header click handler function.
+    function clickHandler( targetElement, sortOptions ) {
+
+        // Get the header element and check if it should be sortable.
+        var thElement = closestParentOrSelf( targetElement, 'th' );
+        if ( !thElement.getAttribute( 'data-sort-property' ) )
+            return;
+
+        // Remove sorting classes.
+        var trElement = closestParentOrSelf( thElement, 'tr' );
+        for ( var i = 0; i < trElement.children.length; ++i ) {
+            trElement.children[i].className = trElement.children[i].className.replace( /sorting-(asc|desc)/g, '' );
+        }
+
+        // Get the options object and set it to one of three states,
+        // depending on the previous sort column/direction:
+        // - null, or different column: set it to ascending on the current column
+        // - ascending: set it to descending
+        // - descending: set it to null
+        var currentSortOptions = sortOptions();
+        if ( !currentSortOptions || currentSortOptions.columnIndex !== thElement.cellIndex ) {
+            sortOptions( {
+                direction: 'asc',
+                columnIndex: thElement.cellIndex,
+                propertyName: thElement.getAttribute( 'data-sort-property' )
+            } );
+        } else if ( currentSortOptions.direction === 'asc' ) {
+            currentSortOptions.direction = 'desc';
+            sortOptions.valueHasMutated();
+        } else {
+            sortOptions( null );
+        }
+
+        // Get the updated options and add CSS class to the related
+        // header element if sorting is active.
+        var newSortOptions = sortOptions();
+        if ( !!newSortOptions ) {
+            var classes = thElement.className.split( ' ' );
+            classes.push( 'sorting-' + newSortOptions.direction );
+            thElement.className = ko.utils.stringTrim( classes.join( ' ' ) );
+        }
+    }
 
     ko.bindingHandlers.tablesortForeach = {
 
@@ -33,22 +105,28 @@
                 throw new Error( 'Malformed table markup: no parent <table> element found for this <tbody> element.' );
 
             // ... And make sure that parent table element has a thead element.
-            var theadElement;
+            var theadElement = null;
             for ( var i = 0; i < tableElement.children.length; ++i ) {
                 if ( tableElement.children[ i ].nodeName.toLowerCase() === 'thead' ) {
                     theadElement = tableElement.children[ i ];
                     break;
                 }
             }
-            if ( !theadElement )
-                throw new Error( 'Malformed table markup: no <thead> element found in this <table> element.' );
+            if ( !theadElement || theadElement.children.length === 0 )
+                throw new Error( 'Malformed table markup: no non-empty <thead> element found in this <table> element.' );
+            
+            // Finally, get the first row in the thead element, which is where
+            // the click handler will be attached.
+            var trElement = theadElement.children[0];
+            if ( trElement.nodeName.toLowerCase() !== 'tr' )
+                throw new Error( 'Malformed table markup: no first <tr> element found in the <thead> element of this table.' );
 
             // Attach a sorted observable to the observable array.
             valueAccessor.sorted = ko.computed( {
                 
                 read: function () {
                     
-                    var unsortedArray = valueAccessor();
+                    var unsortedArray = ko.unwrap( valueAccessor() );
                     var sortOptions = valueAccessor.sorted.options();
 
                     // If the sort options are null or the array is empty,
@@ -85,6 +163,17 @@
             // Initialize the options object for the sort computed.
             valueAccessor.sorted.options = ko.observable( null );
 
+            var clickWrapper = function ( mouseEvent ) {
+                return clickHandler( !!mouseEvent.target ? mouseEvent.target : mouseEvent.srcElement, valueAccessor.sorted.options );
+            };
+
+            // Attach the click handler.
+            if ( trElement.addEventListener ) {
+                trElement.addEventListener( 'click', clickWrapper, false );
+            } else {
+                trElement.attachEvent( 'onclick', clickWrapper );
+            }
+            
             return ko.bindingHandlers.foreach.init( tbodyElement, valueAccessor.sorted, allBindings, viewModel, bindingContext );
         },
         
@@ -96,4 +185,4 @@
         },
     };
 
-} )( ko );
+} )();
